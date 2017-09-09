7a6890f27ea20a813d239a26b951c5846acc57aa
Marc de Klerk <deklerkmc@gmail.com>
2017-09-09 12:25:08 +0200

    Refactored question creation tests
    Extracted redundant addQuestion logic into helper function (in helper library)

--- test/helpers.js
+++ test/helpers.js
@@ -1,14 +1,52 @@
+const acc = web3.eth.accounts
+const admin = acc[1]
+const trusted = acc[2]
+const nonadmin = acc[3]
+
 module.exports = {
-  addQuestion: function (instance, from, question, initialPosition, trustedSource, timeOfBetClose, resolutionDeadlineTime, winningsClaimDeadline) {
-	  return instance.addQuestion(
-	    question, initialPosition, trustedSource, timeOfBetClose, resolutionDeadlineTime, winningsClaimDeadline,
-	    {from: from, value: 3, gas: 3000000}
-	  )
+  addQuestion: function ({instance, question, initialPosition, trustedSource, timeOfBetClose, resolutionDeadlineTime, winningsClaimDeadline, from, value, gas}) {
+    return instance.addQuestion(
+      question, initialPosition, trustedSource, timeOfBetClose, resolutionDeadlineTime, winningsClaimDeadline,
+      {from: from, value: 3, gas: 3000000}
+    )
   },
 
   handleError: function (e) {
-	  if ((e + '').indexOf('invalid JUMP') || (e + '').indexOf('out of gas') || (e + '').indexOf('invalid opcode') > -1) {} else if ((e + '').indexOf('please check your gas amount') > -1) {} else {
-	    throw e
-	  }
+    if ((e + '').indexOf('invalid JUMP') || (e + '').indexOf('out of gas') || (e + '').indexOf('invalid opcode') > -1) {} else if ((e + '').indexOf('please check your gas amount') > -1) {} else {
+      throw e
+    }
+  },
+
+  accounts: function () {
+    return {admin, trusted, nonadmin}
+  },
+
+  createDefaultQuestionParam: function (instance) {
+    now = Date.now()
+
+    from = admin
+
+    question = 'Who will win the World Cup?'
+    initialPosition = [1, 2]
+    trustedSource = trusted
+    timeOfBetClose = now + 10 * 60 * 1000
+    resolutionDeadlineTime = now + 20 * 60 * 1000
+    winningsClaimDeadline = now + 30 * 60 * 1000
+
+    value = 3
+    gas = 3000000
+
+    return {
+      instance,
+      question,
+      initialPosition,
+      trustedSource,
+      timeOfBetClose,
+      resolutionDeadlineTime,
+      winningsClaimDeadline,
+      from,
+      value,
+      gas
+    }
   }
 }
--- test/questions.js
+++ test/questions.js
@@ -3,30 +3,23 @@ const helpers = require('./helpers')
 
 const addQuestion = helpers.addQuestion
 const handleError = helpers.handleError
+const createDefaultQuestionParam = helpers.createDefaultQuestionParam
+const acc = helpers.accounts()
 
 contract('PredicationMarket', account => {
-  const acc = web3.eth.accounts
-  const admin = acc[1]
-  const trusted = acc[2]
-  const nonadmin = acc[3]
-
   var instance
+  var defaultQuestion
 
   beforeEach(async function () {
-    instance = await PredictionMarket.new({from: admin})
+    instance = await PredictionMarket.new({from: acc.admin})
+    defaultQuestion = createDefaultQuestionParam(instance)
   })
 
   describe('Add Question', () => {
-    question = 'What?'
-    questionHash = web3.sha3(question)
-    now = Date.now()
-    timeOfBetClose = now + 10 * 60 * 1000
-    resolutionDeadlineTime = now + 20 * 60 * 1000
-    winningsClaimDeadline = now + 30 * 60 * 1000
-
     it('should allow an administrator to create a question', async function () {
-      await addQuestion(instance, admin, question, [1, 2], acc[1], timeOfBetClose, resolutionDeadlineTime, winningsClaimDeadline)
+      await addQuestion(defaultQuestion)
 
+      questionHash = web3.sha3(defaultQuestion.question)
       let q = await instance.questions(questionHash)
 
       assert.equal(q[0], question, 'Question was not added.')
@@ -34,21 +27,25 @@ contract('PredicationMarket', account => {
 
     describe('should throw if', () => {
       it('question exists', async function () {
-        await addQuestion(instance, admin, question, [1, 2], acc[1], timeOfBetClose, resolutionDeadlineTime, winningsClaimDeadline)
+        await addQuestion(defaultQuestion)
 
-        return await addQuestion(instance, admin, question, [1, 2], acc[1], timeOfBetClose, resolutionDeadlineTime, winningsClaimDeadline)
+        return await await addQuestion(defaultQuestion)
         .catch(e => handleError(e))
       })
 
       // transaction sent from nonadmin
       it('non-administrator to create a question', async function () {
-        return await addQuestion(instance, nonadmin, question, [1, 2], acc[1], timeOfBetClose, resolutionDeadlineTime, winningsClaimDeadline)
+        question = defaultQuestion
+        question.from = acc.nonadmin
+        return await addQuestion(question)
         .catch(e => handleError(e))
       })
 
       // resolutionDeadlineTime is set to current time i.e. before timeOfBetClose
       it('time of bet close is after resolution deadline', async function () {
-        return await addQuestion(instance, nonadmin, question, [1, 2], acc[1], timeOfBetClose, now, winningsClaimDeadline)
+        question = defaultQuestion
+        question.resolutionDeadlineTime = question.timeOfBetClose - 1
+        return await addQuestion(question)
         .catch(e => handleError(e))
       })
     })
