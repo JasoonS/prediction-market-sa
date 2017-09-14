de377bba51eb4926eb4075d6efcb8be25f9df474
Marc de Klerk <deklerkmc@gmail.com>
2017-09-14 08:25:29 +0200

    Added more tests to add question

--- test/questions.js
+++ test/questions.js
@@ -17,12 +17,34 @@ contract('PredicationMarket', account => {
 
   describe('Add Question', () => {
     it('should allow an administrator to create a question', async function () {
-      await addQuestion(defaultQuestionParams)
+      const tx = await addQuestion(defaultQuestionParams)
+
+      questionHash = web3.sha3(defaultQuestionParams.question)
+
+      args = tx.logs[0].args
+      assert.equal(args.questionStatement,                 defaultQuestionParams.question,               "Parameter did not match")
+      assert.equal(args.questionId,                        questionHash,                                 "Parameter did not match")
+      assert.equal(args.inFavour.toString(),               defaultQuestionParams.initialPosition[0],     "Parameter did not match")
+      assert.equal(args.against.toString(),                defaultQuestionParams.initialPosition[1],     "Parameter did not match")
+      assert.equal(args.timeOfBetClose.toString(),         defaultQuestionParams.timeOfBetClose,         "Parameter did not match")
+      assert.equal(args.resolutionDeadlineTime.toString(), defaultQuestionParams.resolutionDeadlineTime, "Parameter did not match")
+      assert.equal(args.winningsClaimDeadline.toString(),  defaultQuestionParams.winningsClaimDeadline,  "Parameter did not match")
+      assert.equal(args.trustedSource,                     defaultQuestionParams.trustedSource,          "Parameter did not match")
 
       questionHash = web3.sha3(defaultQuestionParams.question)
       let q = await instance.questions(questionHash)
 
-      assert.equal(q[0], question, 'Question was not added.')
+      assert.equal(q[0], defaultQuestionParams.question,               "Parameter did not match")
+      assert.equal(q[1], defaultQuestionParams.initialPosition[0],     "Parameter did not match")
+      assert.equal(q[2], defaultQuestionParams.initialPosition[1],     "Parameter did not match")
+      assert.equal(q[3], defaultQuestionParams.timeOfBetClose,         "Parameter did not match")
+      assert.equal(q[4], defaultQuestionParams.resolutionDeadlineTime, "Parameter did not match")
+      assert.equal(q[5], defaultQuestionParams.winningsClaimDeadline,  "Parameter did not match")
+      assert.equal(q[6], defaultQuestionParams.trustedSource,          "Parameter did not match")
+      assert.equal(q[7], true,                                         "Parameter did not match")
+      assert.equal(q[9], false,                                        "Parameter did not match")
+      assert.equal(q[8], 0,                                            "Parameter did not match")
+      assert.equal(q[9], false,                                        "Parameter did not match")
     })
 
     describe('should throw if', () => {
